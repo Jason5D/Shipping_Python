758c20a2d943f61ab815d566ea3cefb9e4cfe666
Jason <jason5donoghue@gmail.com>
2023-09-12 21:08:49 +0100

    all shipping costs

--- shippingCalc.py
+++ shippingCalc.py
@@ -1,13 +1,29 @@
-weight = 8.4
+weight = 4.8
 
 # Ground Shipping
 if (weight <= 2):
-    cost = weight * 1.5 + 20
+    groundCost = weight * 1.5 + 20
 elif (weight <= 6):
-    cost = weight * 3.0 + 20
+    groundCost = weight * 3.0 + 20
 elif (weight <= 10):
-    cost = weight * 4.0 + 20
+    groundCost = weight * 4.0 + 20
 else:
-    cost = weight * 4.75 + 20
+    groundCost = weight * 4.75 + 20
 
-print(cost)
+print("Ground Cost: " + str(groundCost))
+
+# Premium Shipping
+premium = 125
+print("Premium Cost: " + str(premium))
+
+# Drone Shipping
+if (weight <= 2):
+    droneCost = weight * 4.5
+elif (weight <= 6):
+    droneCost = weight * 9
+elif (weight <= 10):
+    droneCost = weight * 12
+else:
+    droneCost = weight * 14.25
+
+print("Drone Cost: " + str(droneCost))
